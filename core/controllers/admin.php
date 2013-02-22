83c3a8253ba7af2f9a9db1879e38ea183cfbca4c
Jean-Philippe Collette <jeanphilippe.collette@gmail.com>
2013-02-22 21:10:05 +0100

    Move commands if the adminpanel is not requested

--- core/controllers/admin.php
+++ core/controllers/admin.php
@@ -11,6 +11,10 @@ class AdminPanel extends Page
 	{
 		$this->setContainer(TPL."html/container.html");
 		$this->setTitle("Admin Menu");
+	}
+	
+	public function prerender()
+	{
 		$this->setTemplate(PATH_TPL_COMMON."html/admin.html");
 		$this->assign('array_plugin', Plugins::getAdminInfos());
 		JPHP::addOnloadFunction("adminInitScript()");
