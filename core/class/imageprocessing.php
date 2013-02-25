d10fc61258073923ace55b713a09a0558b7715ad
Jean-Philippe Collette <jeanphilippe.collette@gmail.com>
2013-02-26 00:33:49 +0100

    Remove useless debug messages

--- core/class/imageprocessing.php
+++ core/class/imageprocessing.php
@@ -14,7 +14,6 @@ class ImageProcessing
 		if(!file_exists($file))
 		{
 			Messages::add("Crop: file " . $file . " does not exist.", Message::ERROR);
-			//Log::add("Crop: file " . $file . " does not exist.");
 			return false;
 		}
 		$array_dim = getimagesize($file);
@@ -27,7 +26,6 @@ class ImageProcessing
 		if($width < $maxWidth || $height < $maxHeight)
 		{
 			Messages::add("Crop: file too small.", Message::ERROR);
-			//Log::add("Crop: file ".$file." too small.");
 			return false;
 		}
 			
