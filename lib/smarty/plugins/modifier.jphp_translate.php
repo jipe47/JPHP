a14c13c96c8a4846946c810f48cf7d928726c391
Jean-Philippe Collette <jeanphilippe.collette@gmail.com>
2013-03-17 00:21:38 +0100

    Quiet mode

--- lib/smarty/plugins/modifier.jphp_translate.php
+++ lib/smarty/plugins/modifier.jphp_translate.php
@@ -1,8 +1,9 @@
 <?php
-function smarty_modifier_jphp_translate($string)
+function smarty_modifier_jphp_translate($string, $quiet="none")
 {
+	$isQuiet = $quiet == "quiet";
 	 if(class_exists("Translator"))
-    	return Translator::translate($string);
+    	return Translator::translate($string, $isQuiet);
     else 
     	return "Undefined Translator";
 }
