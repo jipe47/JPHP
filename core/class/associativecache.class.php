c0478126d677bcbac26152c8443eb16150b23912
Jean-Philippe Collette <jeanphilippe.collette@gmail.com>
2013-07-06 13:26:05 +0200

    get and onMiss methods

--- core/class/associativecache.class.php
+++ core/class/associativecache.class.php
@@ -5,4 +5,41 @@ abstract class AssociativeCache extends Cache2
 	{
 		
 	}
+	
+	public function get()
+	{
+		$arg = func_get_args();
+		$argc = func_num_args();
+		$this->loadCache($arg);
+		
+		if(func_num_args() == 0)
+			return $this->data;
+				
+		// Traverse the array and retreive the desired value
+		$c = $this->data;
+		$alreadyMiss = false;
+		
+		for($i = 0 ; $i < $argc ; $i++)
+		{
+			$a = $arg[$i];
+			if(!array_key_exists($a, $c))
+			{
+				if($alreadyMiss)
+				{
+					throw new Exception("Cache miss failed");
+					return null;
+				}
+				$this->onMiss($arg);
+				$i = -1;
+				$c = $this->data;
+				$alreadyMiss = true;
+				continue;
+			}
+			$c = $c[$a];
+		}
+		
+		return $c;
+	}
+	
+	abstract public function onMiss();
 }
