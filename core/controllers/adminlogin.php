c3144a084e577d8231836c02d82bcc50568ee61c
Jean-Philippe Collette <jeanphilippe.collette@gmail.com>
2013-08-10 20:36:49 +0200

    Stupid login and password

--- core/controllers/adminlogin.php
+++ core/controllers/adminlogin.php
@@ -8,8 +8,7 @@
 class AdminLogin extends Page
 {
 	private $members = array(
-		"jipe47" => "fa9edb67c6a834dd6ffc97b0f03a5fce"
-	
+			"admin" => "21232f297a57a5a743894a0e4a801fc3" // <=> admin
 	);
 	
 	public function construct()
